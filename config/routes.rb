7d255eafd5ba451ee54e2f1577d113378a6fb577
lortega <luis.ortega.araneda@gmail.com>
2013-05-08 13:46:41 -0400

    Se corrigen las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Wolly::Application.routes.draw do
+  get "fb_app_example/nofans"
+
+  get "fb_app_example/index"
+
+  get "fb_app_example/share"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
