6696bf121f2390397bca5752c98dae283780cd88
lortega <luis.ortega.araneda@gmail.com>
2013-05-08 12:02:53 -0400

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolly::Application.config.secret_token = 'a3ab06b1ce8866d2d0d28416a1fdc9affc6126f05ae771f83f08fc09e8c4277dbc09d171e303225e9fbc4b84aea3d42d6fa1c84c54a7052b3c94cde8513f61db'
