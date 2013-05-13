542de53ada499c44b48e6b3e997658518b8aa891
lortega <luis.ortega.araneda@gmail.com>
2013-05-13 10:29:38 -0400

    Se crea la app Claro Ganate la Parabolica

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Wolly::Application.routes.draw do
-  get "fb_app_example/nofans"
+  get "fb_app_claro_ganate_la_parabolica/nofans"
 
-  get "fb_app_example/index"
+  get "fb_app_claro_ganate_la_parabolica/index"
 
-  get "fb_app_example/share"
+  get "fb_app_claro_ganate_la_parabolica/share"
+
+  post "fb_app_example" => "fb_app_example#index"
+  
+  post "fb_app_example/canvas"
+
+  get "fb_app_example/share", :as => :fb_app_example_share
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
