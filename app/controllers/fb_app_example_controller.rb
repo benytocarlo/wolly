2058dce4fcc2b66a6ff94f2dd7884975c7dd2c86
lortega <luis.ortega.araneda@gmail.com>
2013-05-13 10:20:30 -0400

    Se crea metodo para manejar Facebook

--- app/controllers/fb_app_example_controller.rb
+++ app/controllers/fb_app_example_controller.rb
@@ -1,8 +1,20 @@
 class FbAppExampleController < ApplicationController
-  def nofans
+  layout "fb_app_example"
+  before_filter :parse_facebook_signed_request
+
+  def parse_facebook_signed_request
+    @app_id = '433252186786179'
+    @app_secret = '18587ca55978411e37a7f67f35f99c87'
+    @scope = 'email,read_stream,publish_stream,user_photos'
+    session[:signed_request] ||= Koala::Facebook::OAuth.new(@app_id,@app_secret).parse_signed_request(params[:signed_request]).symbolize_keys!
+    @graph = Koala::Facebook::API.new(session[:signed_request][:oauth_token])
   end
 
   def index
+    @me = @graph.get_object("me")
+  end
+
+  def canvas
   end
 
   def share
