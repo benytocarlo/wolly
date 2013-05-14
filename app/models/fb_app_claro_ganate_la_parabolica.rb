bbc7033fb1e48c7c1603a4849679c60a8be0daf2
lortega <luis.ortega.araneda@gmail.com>
2013-05-14 13:30:13 -0400

    Se crea el modelo para la aplicacion de ganate la parabolica

---
+++ app/models/fb_app_claro_ganate_la_parabolica.rb
@@ -0,0 +1,3 @@
+class FbAppClaroGanateLaParabolica < ActiveRecord::Base
+  attr_accessible :eighth, :eleventh, :fifteenth, :fifth, :first, :fourteenth, :fourth, :ninth, :second, :seventh, :sixth, :tenth, :third, :thirteenth, :twelfth
+end
