500b590a54315f23bfaee4ee311383352e64c499
lortega <luis.ortega.araneda@gmail.com>
2013-05-15 18:33:24 -0400

    Se agrega el attr accessor para facebook_idnumber

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,7 @@
 class Participant < ActiveRecord::Base
   has_many :participations
   has_many :applications, :through => :participations
-  validates_presence_of :facebook_id
-  validates_uniqueness_of :facebook_id
-  attr_accessible :address, :city, :country, :facebook_first_name, :facebook_gender, :facebook_id, :facebook_last_name, :facebook_locale, :facebook_middle_name, :facebook_name, :occupation, :phone, :province, :rut
+  validates_presence_of :facebook_idnumber
+  validates_uniqueness_of :facebook_idnumber
+  attr_accessible :address, :city, :country, :facebook_first_name, :facebook_gender, :facebook_idnumber, :facebook_last_name, :facebook_locale, :facebook_middle_name, :facebook_name, :occupation, :phone, :province, :rut
 end
