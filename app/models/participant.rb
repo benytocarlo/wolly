8c55d7986fe035285cfd84b77c210066f2e28ae0
lortega <luis.ortega.araneda@gmail.com>
2013-05-15 18:43:20 -0400

    Se agregan las nuevas seed que son la bd de Padrino

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,5 +3,5 @@ class Participant < ActiveRecord::Base
   has_many :applications, :through => :participations
   validates_presence_of :facebook_idnumber
   validates_uniqueness_of :facebook_idnumber
-  attr_accessible :address, :city, :country, :facebook_first_name, :facebook_gender, :facebook_idnumber, :facebook_last_name, :facebook_locale, :facebook_middle_name, :facebook_name, :occupation, :phone, :province, :rut
+  attr_accessible :address, :city, :country, :facebook_first_name, :facebook_gender, :facebook_idnumber, :facebook_last_name, :facebook_locale, :facebook_middle_name, :facebook_name, :occupation, :phone, :province, :rut, :facebook_email
 end
