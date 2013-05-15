8707678f97021ecd246c403762ae4900dc2c4b8d
lortega <luis.ortega.araneda@gmail.com>
2013-05-15 19:27:36 -0400

    mod

--- app/admin/participants.rb
+++ app/admin/participants.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Participant do
-  
+  index do                            
+    column :facebook_idnumber
+    column :facebook_email                 
+    column :facebook_name        
+    column :facebook_gender           
+    column :rut
+    column :address
+    column :occupation
+    column :city
+    column :province
+    column :country           
+    default_actions                   
+  end
 end
