4222637d51deed6f4ecf11f9ca806b2f53a10dda
lortega <luis.ortega.araneda@gmail.com>
2013-05-20 12:45:11 -0400

    Se crea un test unitario

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -4,4 +4,9 @@ class ParticipantTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "should not save participant without facebook_idnumber" do
+    participant = Participant.new
+    assert !participant.save, "Saved the participant without the facebook_idnumber"
+  end
 end
