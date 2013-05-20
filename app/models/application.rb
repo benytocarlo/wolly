5d6fe56fc26e6cdc8ea916f579eff843accd2103
lortega <luis.ortega.araneda@gmail.com>
2013-05-20 12:13:58 -0400

    Se hacen correcciones para poder mostrar el GA sólo cuando ha sido explicito

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,5 +3,5 @@ class Application < ActiveRecord::Base
   has_many :participants, :through => :participations
   validates_presence_of :fb_app_idnumber
   validates_uniqueness_of :fb_app_idnumber
-  attr_accessible :fb_app_idnumber, :fb_app_secret, :invite_message, :name, :share_caption, :share_description
+  attr_accessible :fb_app_idnumber, :fb_app_secret, :invite_message, :name, :share_caption, :share_description, :ga
 end
