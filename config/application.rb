1147535ce70f934f178a7b187c400a9ce8f63d02
lortega <luis.ortega.araneda@gmail.com>
2013-05-27 19:21:26 -0400

    Se agrega la misma hoja de estilos pero en SCSS

--- config/application.rb
+++ config/application.rb
@@ -57,8 +57,6 @@ module Wolly
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
-    config.assets.precompile += ['.css', '.js']
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
