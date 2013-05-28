225fa878ecb05778dfd8906df6d11d718600bbbf
lortega <luis.ortega.araneda@gmail.com>
2013-05-28 10:53:52 -0400

    Se hacen seleccionables los elementos de participantes en el administrador

--- app/admin/participants.rb
+++ app/admin/participants.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register Participant do
   menu :label => "Participantes"
   
-  index do                            
+  index do                       
+    selectable_column
     column :facebook_idnumber
     column :facebook_email                 
     column :facebook_name                 
