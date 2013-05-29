11cb1e761e574899416a7a997200ccfe238ad61d
lortega <luis.ortega.araneda@gmail.com>
2013-05-29 18:31:08 -0400

    Se comentan los métodos del Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,22 +20,25 @@ private
     session[:signed_request] ||= Koala::Facebook::OAuth.new(@app_id,@app_secret).parse_signed_request(params[:signed_request]).deep_symbolize_keys
   end
 
-  # Guarda las Cookies del usuario en @facebook_coookies.
-  # Este método se debe llamar sólo cuando la aplicación
-  # haya sido aceptada por el usuario, de lo contrario
-  # no encontrará las Cookies y dará error.
+  # Guarda las Cookies del usuario en @facebook_coookies. Este método se debe llamar sólo cuando la aplicación
+  # haya sido aceptada por el usuario, de lo contrario no encontrará las Cookies y dará error.
   #
   def parse_facebook_cookies
     session[:facebook_cookies] ||= Koala::Facebook::OAuth.new(@app_id,@app_secret).get_user_info_from_cookie(cookies).deep_symbolize_keys
   end
 
-  # Crea un objeto de tipo Graph con el que se puede conversar con Facebook.
-  #
+  # Instancia un objeto de clase Koala con el que se puede conversar con Facebook: @graph.
+  # 
   def load_graph_api
-    unless session[:facebook_cookies].nil? 
+    unless session[:facebook_cookies].nil?
+      # Si el usuario ha aceptado la aplicación, Facebook nos enviará
+      # un Access Token en las Cookies; usándolo se instanciará el objeto
+      # Graph que puede conversar con Facebook.
       @access_token = session[:facebook_cookies][:access_token]
       @graph = Koala::Facebook::API.new(@access_token)
     else
+      # En otro caso, se puede instanciar el objeto Graph sin un Access
+      # Token; sin un Access Token, habrá datos que no se pueden recuperar.
       @graph = Koala::Facebook::API.new
     end
   end  
