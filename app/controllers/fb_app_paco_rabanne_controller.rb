1e56b30be31b1404119fe956933db9307ffbb1f1
Pablo Stuardo <pablo.stuardo.diaz@gmail.com>
2013-05-29 19:12:44 -0400

    porgramacion de formulario

--- app/controllers/fb_app_paco_rabanne_controller.rb
+++ app/controllers/fb_app_paco_rabanne_controller.rb
@@ -27,14 +27,15 @@ class FbAppPacoRabanneController < ApplicationController
   end
 
   def share
+    @app_id = "426276337470229"
     if params[:nombre].present? and params[:email].present? and params[:rut].present? and params[:celular].present?
         @identificador_user = "rabanne-"+params[:rut]
 
       if @me_from_database = Participant.find_by_facebook_idnumber(@identificador_user)
-        @me_from_database.update_attributes(:facebook_name => params[:nombre]+" "+params[:apellido], :facebook_email => params[:email], :rut => params[:rut], :phone => params[:celular], :address => params[:direccion], :city => params[:region], :province => params[:comuna], :facebook_gender => params[:sexo])
+        #@me_from_database.update_attributes(:facebook_name => params[:nombre]+" "+params[:apellido], :facebook_email => params[:email], :rut => params[:rut], :phone => params[:celular], :address => params[:direccion], :city => params[:region], :province => params[:comuna], :facebook_gender => params[:sexo])
         Participation.create(:application_id => @app.id, :participant_id => @me_from_database.id, :answer => "Participando")
       else
-         @me_from_database = Participant.create(:facebook_idnumber => @identificador_user ,:facebook_name => params[:nombre]+" "+params[:apellido], :facebook_email => params[:email], :rut => params[:rut], :phone => params[:celular], :address => params[:direccion], :city => params[:region], :province => params[:comuna], :facebook_gender => params[:sexo])
+        @me_from_database = Participant.create(:facebook_idnumber => @identificador_user ,:facebook_name => params[:nombre]+" "+params[:apellido], :facebook_email => params[:email], :rut => params[:rut], :phone => params[:celular], :address => params[:direccion], :city => params[:region], :province => params[:comuna], :facebook_gender => params[:sexo])
         @fecha = params[:day]+"-"+params[:month]+"-"+params[:year]
         Participation.create(:application_id => "426276337470229", :participant_id => @me_from_database.id, :answer => params[:codigo_boleta]+"/"+@fecha)
       end
