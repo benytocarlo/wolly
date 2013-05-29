fa31a7ad268db58f8d1ae4c5a4484f2c8670b91e
lortega <luis.ortega.araneda@gmail.com>
2013-05-29 19:03:49 -0400

    Indentación

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,59 @@
 Wolly::Application.routes.draw do
   post "fb_app_mahindra_reforestemos" => "fb_app_mahindra_reforestemos#index"
 
-  post "fb_app_brilliance_chile"                  => "fb_app_brilliance_chile#index"
-  post "fb_app_brilliance_chile/canvas"
-  get "fb_app_brilliance_chile/video"             => "fb_app_brilliance_chile#video"
-  get "fb_app_brilliance_chile/especificaciones"  => "fb_app_brilliance_chile#especificaciones"
-  get "fb_app_brilliance_chile/concurso"          => "fb_app_brilliance_chile#concurso"           , :as => :perro 
-  post "fb_app_brilliance_chile/share"            => "fb_app_brilliance_chile#share"              , :as => :fb_app_brilliance_chile_share
-  get "fb_app_brilliance_chile/new_participant"   => "fb_app_brilliance_chile#new_participant"
-
-
-  post "fb_app_claro_ganate_la_parabolica"             => "fb_app_claro_ganate_la_parabolica#index"
-  get  "fb_app_claro_ganate_la_parabolica"             => "fb_app_claro_ganate_la_parabolica#index"
-  get "fb_app_claro_ganate_la_parabolica_premios"      => "fb_app_claro_ganate_la_parabolica#premios"
-  get "fb_app_claro_ganate_la_parabolica_registro"     => "fb_app_claro_ganate_la_parabolica#registro"
-  post "fb_app_claro_ganate_la_parabolica_registro"    => "fb_app_claro_ganate_la_parabolica#procesar_registro"
-  get "fb_app_claro_ganate_la_parabolica_ranking"      => "fb_app_claro_ganate_la_parabolica#ranking"
-  get "fb_app_claro_ganate_la_parabolica_laparabolica" => "fb_app_claro_ganate_la_parabolica#laparabolica"
-  get "fb_app_claro_ganate_la_parabolica_bases"        => "fb_app_claro_ganate_la_parabolica#bases"
-  post "fb_app_claro_ganate_la_parabolica/canvas"
-  get "fb_app_claro_ganate_la_parabolica/pregunta"
-  get "fb_app_claro_ganate_la_parabolica/vota"
-  post "fb_app_claro_ganate_la_parabolica/vota"        => "fb_app_claro_ganate_la_parabolica#procesar_voto"
-  get "fb_app_claro_ganate_la_parabolica/share", :as => :fb_app_claro_ganate_la_parabolica_share
-
-  get "fb_app_paco_rabanne"             => "fb_app_paco_rabanne#index"
-  get "fb_app_paco_rabanne/concurso"
-  post "fb_app_paco_rabanne/share"
-  get "fb_app_paco_rabanne/comunas"
+  post   "fb_app_brilliance_chile"                  => "fb_app_brilliance_chile#index"
+  post   "fb_app_brilliance_chile/canvas"
+  get    "fb_app_brilliance_chile/video"             => "fb_app_brilliance_chile#video"
+  get    "fb_app_brilliance_chile/especificaciones"  => "fb_app_brilliance_chile#especificaciones"
+  get    "fb_app_brilliance_chile/concurso"          => "fb_app_brilliance_chile#concurso"           , :as => :perro 
+  post   "fb_app_brilliance_chile/share"            => "fb_app_brilliance_chile#share"              , :as => :fb_app_brilliance_chile_share
+  get    "fb_app_brilliance_chile/new_participant"   => "fb_app_brilliance_chile#new_participant"
   
-  post "fb_app_mahindra_xuv"                  => "fb_app_mahindra_xuv#index"
-  post "fb_app_mahindra_xuv/canvas"
-  get "fb_app_mahindra_xuv/video"             => "fb_app_mahindra_xuv#video"
-  get "fb_app_mahindra_xuv/especificaciones"  => "fb_app_mahindra_xuv#especificaciones"
-  get "fb_app_mahindra_xuv/concurso"          => "fb_app_mahindra_xuv#concurso"
-  post "fb_app_mahindra_xuv/share"            => "fb_app_mahindra_xuv#share"              , :as => :fb_app_mahindra_xuv_share
-  get "fb_app_mahindra_xuv/new_participant"   => "fb_app_mahindra_xuv#new_participant"
-  
-  post "fb_app_sernatur_capture"                  => "fb_app_sernatur_capture#index"
-  post "fb_app_sernatur_capture/canvas"
-  get "fb_app_sernatur_capture/video"             => "fb_app_sernatur_capture#video"
-  get "fb_app_sernatur_capture/especificaciones"  => "fb_app_sernatur_capture#especificaciones"
-  get "fb_app_sernatur_capture/concurso"          => "fb_app_sernatur_capture#concurso"
-  post "fb_app_sernatur_capture/share"            => "fb_app_sernatur_capture#share"              , :as => :fb_app_sernatur_capture_share
-  get "fb_app_sernatur_capture/new_participant"   => "fb_app_sernatur_capture#new_participant"
+  get    "fb_app_claro_ganate_la_parabolica"              => "fb_app_claro_ganate_la_parabolica#index"
+  post   "fb_app_claro_ganate_la_parabolica"              => "fb_app_claro_ganate_la_parabolica#index"
+  get    "fb_app_claro_ganate_la_parabolica/premios"
+  get    "fb_app_claro_ganate_la_parabolica/registro"
+  post   "fb_app_claro_ganate_la_parabolica/registro"     => "fb_app_claro_ganate_la_parabolica#procesar_registro"
+  get    "fb_app_claro_ganate_la_parabolica/ranking"
+  get    "fb_app_claro_ganate_la_parabolica/laparabolica"
+  get    "fb_app_claro_ganate_la_parabolica/bases"
+  post   "fb_app_claro_ganate_la_parabolica/canvas"
+  get    "fb_app_claro_ganate_la_parabolica/pregunta"
+  get    "fb_app_claro_ganate_la_parabolica/vota"
+  post   "fb_app_claro_ganate_la_parabolica/vota"         => "fb_app_claro_ganate_la_parabolica#procesar_voto"
+  get    "fb_app_claro_ganate_la_parabolica/share"
 
-  post "fb_app_example" => "fb_app_example#index"
-  post "fb_app_example/canvas"
-  get "fb_app_example/share", :as => :fb_app_example_share
+  get    "fb_app_paco_rabanne"             => "fb_app_paco_rabanne#index"
+  get    "fb_app_paco_rabanne/concurso"
+  post   "fb_app_paco_rabanne/share"
+  get    "fb_app_paco_rabanne/comunas"
+  
+  post   "fb_app_mahindra_xuv"                  => "fb_app_mahindra_xuv#index"
+  post   "fb_app_mahindra_xuv/canvas"
+  get    "fb_app_mahindra_xuv/video"             => "fb_app_mahindra_xuv#video"
+  get    "fb_app_mahindra_xuv/especificaciones"  => "fb_app_mahindra_xuv#especificaciones"
+  get    "fb_app_mahindra_xuv/concurso"          => "fb_app_mahindra_xuv#concurso"
+  post   "fb_app_mahindra_xuv/share"            => "fb_app_mahindra_xuv#share"              , :as => :fb_app_mahindra_xuv_share
+  get    "fb_app_mahindra_xuv/new_participant"   => "fb_app_mahindra_xuv#new_participant"
+  
+  post   "fb_app_sernatur_capture"                  => "fb_app_sernatur_capture#index"
+  post   "fb_app_sernatur_capture/canvas"
+  get    "fb_app_sernatur_capture/video"             => "fb_app_sernatur_capture#video"
+  get    "fb_app_sernatur_capture/especificaciones"  => "fb_app_sernatur_capture#especificaciones"
+  get    "fb_app_sernatur_capture/concurso"          => "fb_app_sernatur_capture#concurso"
+  post   "fb_app_sernatur_capture/share"            => "fb_app_sernatur_capture#share"              , :as => :fb_app_sernatur_capture_share
+  get    "fb_app_sernatur_capture/new_participant"   => "fb_app_sernatur_capture#new_participant"
+
+  post   "fb_app_example" => "fb_app_example#index"
+  post   "fb_app_example/canvas"
+  get    "fb_app_example/share", :as => :fb_app_example_share
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
+  
 end
 #== Route Map
-# Generated on 29 May 2013 18:42
+# Generated on 29 May 2013 18:50
 #
 #                        fb_app_brilliance_chile POST       /fb_app_brilliance_chile(.:format)                        fb_app_brilliance_chile#index
 #                 fb_app_brilliance_chile_canvas POST       /fb_app_brilliance_chile/canvas(.:format)                 fb_app_brilliance_chile#canvas
@@ -63,14 +62,14 @@ end
 #                                          perro GET        /fb_app_brilliance_chile/concurso(.:format)               fb_app_brilliance_chile#concurso
 #                  fb_app_brilliance_chile_share POST       /fb_app_brilliance_chile/share(.:format)                  fb_app_brilliance_chile#share
 #        fb_app_brilliance_chile_new_participant GET        /fb_app_brilliance_chile/new_participant(.:format)        fb_app_brilliance_chile#new_participant
-#              fb_app_claro_ganate_la_parabolica POST       /fb_app_claro_ganate_la_parabolica(.:format)              fb_app_claro_ganate_la_parabolica#index
-#                                                GET        /fb_app_claro_ganate_la_parabolica(.:format)              fb_app_claro_ganate_la_parabolica#index
-#      fb_app_claro_ganate_la_parabolica_premios GET        /fb_app_claro_ganate_la_parabolica_premios(.:format)      fb_app_claro_ganate_la_parabolica#premios
-#     fb_app_claro_ganate_la_parabolica_registro GET        /fb_app_claro_ganate_la_parabolica_registro(.:format)     fb_app_claro_ganate_la_parabolica#registro
-#                                                POST       /fb_app_claro_ganate_la_parabolica_registro(.:format)     fb_app_claro_ganate_la_parabolica#procesar_registro
-#      fb_app_claro_ganate_la_parabolica_ranking GET        /fb_app_claro_ganate_la_parabolica_ranking(.:format)      fb_app_claro_ganate_la_parabolica#ranking
-# fb_app_claro_ganate_la_parabolica_laparabolica GET        /fb_app_claro_ganate_la_parabolica_laparabolica(.:format) fb_app_claro_ganate_la_parabolica#laparabolica
-#        fb_app_claro_ganate_la_parabolica_bases GET        /fb_app_claro_ganate_la_parabolica_bases(.:format)        fb_app_claro_ganate_la_parabolica#bases
+#              fb_app_claro_ganate_la_parabolica GET        /fb_app_claro_ganate_la_parabolica(.:format)              fb_app_claro_ganate_la_parabolica#index
+#                                                POST       /fb_app_claro_ganate_la_parabolica(.:format)              fb_app_claro_ganate_la_parabolica#index
+#      fb_app_claro_ganate_la_parabolica_premios GET        /fb_app_claro_ganate_la_parabolica/premios(.:format)      fb_app_claro_ganate_la_parabolica#premios
+#     fb_app_claro_ganate_la_parabolica_registro GET        /fb_app_claro_ganate_la_parabolica/registro(.:format)     fb_app_claro_ganate_la_parabolica#registro
+#                                                POST       /fb_app_claro_ganate_la_parabolica/registro(.:format)     fb_app_claro_ganate_la_parabolica#procesar_registro
+#      fb_app_claro_ganate_la_parabolica_ranking GET        /fb_app_claro_ganate_la_parabolica/ranking(.:format)      fb_app_claro_ganate_la_parabolica#ranking
+# fb_app_claro_ganate_la_parabolica_laparabolica GET        /fb_app_claro_ganate_la_parabolica/laparabolica(.:format) fb_app_claro_ganate_la_parabolica#laparabolica
+#        fb_app_claro_ganate_la_parabolica_bases GET        /fb_app_claro_ganate_la_parabolica/bases(.:format)        fb_app_claro_ganate_la_parabolica#bases
 #       fb_app_claro_ganate_la_parabolica_canvas POST       /fb_app_claro_ganate_la_parabolica/canvas(.:format)       fb_app_claro_ganate_la_parabolica#canvas
 #     fb_app_claro_ganate_la_parabolica_pregunta GET        /fb_app_claro_ganate_la_parabolica/pregunta(.:format)     fb_app_claro_ganate_la_parabolica#pregunta
 #         fb_app_claro_ganate_la_parabolica_vota GET        /fb_app_claro_ganate_la_parabolica/vota(.:format)         fb_app_claro_ganate_la_parabolica#vota
