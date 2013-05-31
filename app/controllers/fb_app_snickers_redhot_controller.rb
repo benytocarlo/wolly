41020af5ff4734f5d129cad6a126be0f8948a0d2
benytocarlo <itoknot2004@gmail.com>
2013-05-31 18:11:52 -0400

    cambio appid prod

--- app/controllers/fb_app_snickers_redhot_controller.rb
+++ app/controllers/fb_app_snickers_redhot_controller.rb
@@ -56,7 +56,7 @@ private
   #
   def load_application_data
     @app_id = '157469834435836' if Rails.env.development?
-    @app_id = '460929383989443' if Rails.env.production?
+    @app_id = '570638559624540' if Rails.env.production?
     @app = Application.find_by_fb_app_idnumber @app_id
     @app_secret = @app.fb_app_secret    
     @scope = 'email,read_stream,publish_stream,user_photos'
