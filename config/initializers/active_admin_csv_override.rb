0d5d60ec406cbf7a28b7b0f6aa05b81cd2513558
lortega <luis.ortega.araneda@gmail.com>
2013-07-09 16:40:39 -0400

    Se baja el numero de registros del CSV de activeadmin

--- config/initializers/active_admin_csv_override.rb
+++ config/initializers/active_admin_csv_override.rb
@@ -2,11 +2,11 @@ module ActiveAdmin
   class ResourceController < BaseController
     module DataAccess
       def max_csv_records
-        150_000
+        15_000
       end
       
       def max_per_page
-        150_000
+        15_000
       end
     end
   end
