df4cd96f7568dbcadd2cbb1346e895c3ae19cdbe
lortega <luis.ortega.araneda@gmail.com>
2013-07-25 12:15:16 -0400

    Cambios hechos al ActiveAdmin para que cumpla con el formato de la version 060

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,38 +2,16 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+  sidebar "Balance Mensual" do
+    table_for Application.order("id desc").limit(10) do 
+      column :app_cost
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
-end
-
-ActiveAdmin::Dashboards.build do  
-  section "Ultimas Aplicaciones" do  
+    span { "Total Mes: "+Application.costototal.to_s}
+    br
+    strong { link_to "Ver Todas las Aplicaciones", admin_applications_path }
+  end
+  
+  content :title => "Ultimas Aplicaciones" do  
     table_for Application.order("id desc").limit(10) do  
       column :name
       column :created_at
@@ -44,16 +22,20 @@ ActiveAdmin::Dashboards.build do
         link_to "Participantes", participants_list_path
       end
     end  
-  end  
+  end
+  
+=begin
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+      span :class => "blank_slate" do
+        span I18n.t("active_admin.dashboard_welcome.welcome")
+        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      end
+    end
+  end
+=end
 end
 
-ActiveAdmin::Dashboards.build do
-  section "Balance Mensual" do
-    table_for Application.order("id desc").limit(10) do 
-      column :app_cost
-    end
-    span { "Total Mes: "+Application.costototal.to_s}
-    br
-    strong { link_to "Ver Todas las Aplicaciones", admin_applications_path }
-  end
-end
+
+
+
