3399165b7e466691e1fe99c7076da30ce3d7ac15
benytocarlo <itoknot2004@gmail.com>
2013-07-29 12:05:49 -0400

    cambio nombre de variable

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -2,11 +2,11 @@
 class ParticipantMailer < ActionMailer::Base
   default from: "concurso@mym.com"
 
-  def mail_mym_4entradas mail
-    mail(:to => mail, :subject => "Ganaste 4 Entradas!")
+  def mail_mym_4entradas (email)
+    mail(:to => email, :subject => "Ganaste 4 Entradas!")
   end
 
-  def mail_mym_1millon mail
-    mail(:to => mail, :subject => "Ganaste 1 Millón!")
+  def mail_mym_1millon (email)
+    mail(:to => email, :subject => "Ganaste 1 Millón!")
   end
 end
